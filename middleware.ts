28763e06e5e15a176ee2328e47d07db0f6aa1e5d
Luís Duarte <l.duarte@runtime-revolution.com>
2025-05-08 11:08:43 +0100

    Fixed auth middleware to send secret in the token

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,10 @@ import type { NextRequest } from 'next/server'
 import { getToken } from 'next-auth/jwt'
 
 export async function middleware(request: NextRequest) {
-  const token = await getToken({ req: request })
+  const token = await getToken({ 
+    req: request,
+    secret: process.env.NEXTAUTH_SECRET
+  })
 
   if (!token) {
     return NextResponse.redirect(new URL('/auth/signin', request.url))
