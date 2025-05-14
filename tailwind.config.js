7f1d7f3bb6fe55b88a848640c54f3423220b8c64
Luís Duarte <l.duarte@runtime-revolution.com>
2025-05-14 11:47:57 +0100

    Fixed tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ["class"],
   content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  prefix: "",
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
     extend: {},
   },
   plugins: [],
